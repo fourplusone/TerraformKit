4ed7c0cdf2b575f8d5a87ee9b16d50553d1504db
Matthias Bartelmeß <mba@fourplusone.de>
2020-09-05 15:40:08 +0200

    [LIC] Add License to Invoke.swift

--- Sources/TerraformKit/Invoke.swift
+++ Sources/TerraformKit/Invoke.swift
@@ -1,4 +1,17 @@
 #if canImport(Darwin) && os(macOS)
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2016, 2018 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+
+// macOS's Foundation does not forward stdin to subprocesses when used from the
+// terminal. Therefore a slightly modified version from swift-corelibs-foundation
+// is used to spawn a process on macOS
+
 import CoreFoundation
 import Foundation
 
